dd5852ca92604f2328fd5f30f7d6629f5a47d77f
evan-moon <bboydart91@gmail.com>
2021-04-25 21:59:19 +0900

    test

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
+import core from '@actions/core';
 import { sendMessage } from 'utils/slack';
 import { TARGET_SLACK_CHANNEL_ID } from 'utils/input';
 
 async function main() {
-  console.log('hi');
-  sendMessage({
-    channel: TARGET_SLACK_CHANNEL_ID,
-    text: '테스트',
-  });
+  try {
+    console.log('hi');
+    sendMessage({
+      channel: TARGET_SLACK_CHANNEL_ID,
+      text: '테스트',
+    });
+  } catch (e) {
+    core.setFailed(e.message);
+  }
 }
 
 main();
