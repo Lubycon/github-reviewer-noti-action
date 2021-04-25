dad1112d32a2767b89bd835d0e4c881b63f596fd
evan-moon <bboydart91@gmail.com>
2021-04-25 21:46:11 +0900

    slack token test

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
+import { sendMessage } from 'utils/slack';
+import { TARGET_SLACK_CHANNEL_ID } from 'utils/input';
+
 async function main() {
   console.log('hi');
+  sendMessage({
+    channel: TARGET_SLACK_CHANNEL_ID,
+    text: '테스트',
+  });
 }
 
 main();
---
+++ utils/slack.ts
@@ -0,0 +1,20 @@
+import { WebClient } from '@slack/web-api';
+import { SLACK_BOT_TOKEN } from 'utils/input';
+
+const slackClient = new WebClient(SLACK_BOT_TOKEN);
+
+export function sendMessage({ channel, text }: { channel: string; text: string }) {
+  return slackClient.chat.postMessage({
+    channel,
+    text,
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text,
+        },
+      },
+    ],
+  });
+}
