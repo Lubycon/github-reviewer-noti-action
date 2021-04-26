ebf12052008f5ecb8ea8b21e774bc955a3d12023
Lubycon Admin <68942150+lubycon-admin@users.noreply.github.com>
2021-04-27 01:21:26 +0900

    feat: 슬랙 아이디로 유저 인증하도록 수정 테슷흐4 (#16)
    * chore: add cicd setup
    
    * feat: 슬랙 유저 아이디로 인증 수행하도록 수정
    
    * feat: 인증된 유저 아이디가 아닐 경우 액션 자체를 실패시키도록 수정
    
    * fix: 잘못된 인풋 네임 수정
    
    * fix: Failed 후 액션이 끝까지 수행되지 않도록 수정
    
    * feat: 올바른 슬랙 아이디로 다시 시도
    
    Co-authored-by: evan-moon <bboydart91@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import * as core from '@actions/core';
 import { createPullRequestReviewMessage, sendMessagePullRequestReviewMessage } from './utils/slack';
 import { getPullRequest, isReadyCodeReview } from './utils/github';
+import { MY_SLACK_USER_ID } from './utils/input';
+import { isValidUser } from './utils/user';
 
 async function main() {
+  if (!isValidUser(MY_SLACK_USER_ID)) {
+    core.setFailed('루비콘 허브 슬랙 워크스페이스에 속한 유저 아이디가 아닙니다');
+    return;
+  }
+
   if (!isReadyCodeReview()) {
     return;
   }
