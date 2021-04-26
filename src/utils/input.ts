92dc49284e20d363a45edc663f962c1ea7e86d6c
evan-moon <bboydart91@gmail.com>
2021-04-27 01:11:41 +0900

    feat: 슬랙 유저 아이디로 인증 수행하도록 수정

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -4,3 +4,4 @@ export const BASE_BRANCH = core.getInput('base-branch');
 export const SLACK_BOT_TOKEN = core.getInput('slack-bot-token');
 export const TARGET_SLACK_CHANNEL_ID = core.getInput('channel-id');
 export const GITHUB_TOKEN = core.getInput('github-token');
+export const MY_SLACK_USER_ID = core.getInput('my-slack-id');
