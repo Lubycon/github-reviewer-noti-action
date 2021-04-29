345e8aa69cb1e303c0e8f34861d71edf2a94c42f
evan-moon <bboydart91@gmail.com>
2021-04-29 23:04:19 +0900

    refactor: 의미없는 값 제거

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -3,4 +3,3 @@ import * as core from '@actions/core';
 export const SLACK_BOT_TOKEN = core.getInput('slack-bot-token');
 export const TARGET_SLACK_CHANNEL_ID = core.getInput('channel-id');
 export const GITHUB_TOKEN = core.getInput('github-token');
-export const MY_SLACK_USER_ID = core.getInput('my-slack-user-id');
--- src/utils/user.ts
+++ src/utils/user.ts
@@ -5,7 +5,3 @@ export function findSlackUserByGithubUser(githubUserName: string) {
   const targets = developers as Developer[];
   return targets.find(user => user.githubUserName === githubUserName);
 }
-
-export function isValidUser(slackUserId: string) {
-  return developers.find(developer => developer.slackUserId === slackUserId) != null;
-}
