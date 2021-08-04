e39546e49f35cb278a5d32fd8c867a090ee89f55
evan-moon <bboydart91@gmail.com>
2021-08-04 23:44:54 +0900

    remove test webhook

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core';
 
 export const GITHUB_TOKEN = core.getInput('github-token');
-// export const MATTERMOST_WEBHOOK_URL = core.getInput('mattermost-webhook');
-export const MATTERMOST_WEBHOOK_URL = 'https://mattermost.lubycon.io/hooks/o5sn395coifhfbkyiwy3j7twyh';
+export const MATTERMOST_WEBHOOK_URL = core.getInput('mattermost-webhook');
