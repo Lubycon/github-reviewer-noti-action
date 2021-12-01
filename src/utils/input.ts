88ea22b32dfbd29f2d90bee2c6d27e1bd2b7b3d4
evan-moon <bboydart91@gmail.com>
2021-12-01 21:01:49 +0900

    remove test code

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -1,9 +1,5 @@
-// import * as core from '@actions/core';
+import * as core from '@actions/core';
 
-// export const SLACK_BOT_TOKEN = core.getInput('slack-bot-token', { required: true });
-// export const TARGET_SLACK_CHANNEL_ID = core.getInput('slack-channel-id', { required: true });
-// export const SLACK_BOT_SIGNING_SECRET = core.getInput('slack-bot-signing-secret', { required: true });
-
-export const SLACK_BOT_TOKEN = 'xoxb-2767028122519-2785512299763-9Ef2ZyiES7WEGINyCdcCynig';
-export const TARGET_SLACK_CHANNEL_ID = 'C02NNS66KUP';
-export const SLACK_BOT_SIGNING_SECRET = 'd09f74ad6d527064ed140778a5ab8a87';
+export const SLACK_BOT_TOKEN = core.getInput('slack-bot-token', { required: true });
+export const TARGET_SLACK_CHANNEL_ID = core.getInput('slack-channel-id', { required: true });
+export const SLACK_BOT_SIGNING_SECRET = core.getInput('slack-bot-signing-secret', { required: true });
