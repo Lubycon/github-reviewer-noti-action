0f4d55c550eea92064345cf4956664ead0df7e5f
evan-moon <bboydart91@gmail.com>
2021-12-01 15:50:52 +0900

    add test log

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import {
   sendPullRequestReviewSlackMessage,
   sendReviewApprovedSlackMessage,
 } from 'utils/slack';
+import { SLACK_BOT_TOKEN } from 'utils/input';
 
 const { eventName, payload } = github.context;
 
@@ -17,6 +18,7 @@ async function main() {
   core.info('🔥 Run.....');
   core.info(`eventName = ${eventName}`);
   core.info(`action = ${payload.action}`);
+  core.info(`token = ${SLACK_BOT_TOKEN.length}`);
 
   if (!SUPPROTED_EVENTS.includes(eventName)) {
     core.warning(`현재 이 액션은 ${SUPPROTED_EVENTS.join(', ')} 이벤트만 지원합니다.`);
